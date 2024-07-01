51cfc44287bceddda257b1e9ea3ccab53e3632b3
zkz098 <zkz20081204@126.com>
2024-07-01 14:03:45 +0800

    feat: clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,18 @@ export default defineConfig({
 	site: "https://docs-hexo.shokax.top",
 	integrations: [
 		starlight({
+			head: [
+				{
+					tag: "script",
+					content: `
+					(function(c,l,a,r,i,t,y){
+					c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+					t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+					y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "n0etdmlwab");
+					`
+				}
+			],
 			defaultLocale: 'root',
 			locales: {
 				'root': {
