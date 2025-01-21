480d4d60676f716bd7517f41ea6e54164a0744db
zkz098 <zkz20081204@126.com>
2025-01-21 21:53:09 +0800

    feat: 主题配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,49 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hexo.docs.shokax.top",
-	integrations: [
-		starlight({
-			head: [
-				{
-					tag: "script",
-					content: `
-					(function(c,l,a,r,i,t,y){
-					c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-					t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-					y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-					})(window, document, "clarity", "script", "n0etdmlwab");
-					`
-				}
-			],
-			defaultLocale: 'root',
-			locales: {
-				'root': {
-					label: '简体中文',
-					lang: 'zh-CN'
-				},
-				'en': {
-					label: 'English',
-					lang: 'en'
-				}
-			},
-			title: 'ShokaX Docs',
-			social: {
-				github: 'https://github.com/theme-shoka-x/shokaX-docs-v2',
-			},
-			sidebar: [
-					{
-						label: '从这里开始',
-						autogenerate: { directory: 'getting-started' },
-					},
-					{
-						label: '特色功能',
-						autogenerate: { directory: 'features' },
-					}
-				
-				],
-		}),
-	],
+    site: "https://hexo.docs.shokax.top",
+    integrations: [starlight({
+        head: [
+            {
+                tag: "script",
+                content: `
+                (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "n0etdmlwab");
+                `
+            }
+        ],
+        defaultLocale: 'root',
+        locales: {
+            'root': {
+                label: '简体中文',
+                lang: 'zh-CN'
+            },
+            'en': {
+                label: 'English',
+                lang: 'en'
+            }
+        },
+        title: 'ShokaX Docs',
+        social: {
+            github: 'https://github.com/theme-shoka-x/shokaX-docs-v2',
+        },
+        sidebar: [
+                {
+                    label: '从这里开始',
+                    autogenerate: { directory: 'getting-started' },
+                },
+                {
+                    label: '特色功能',
+                    autogenerate: { directory: 'features' },
+                }
+            
+            ],
+		}), mdx()],
 });
